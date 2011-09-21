c48f4ca32975af379437b73f2f76ebd2cf7d3a29
Chris Gibbs <cjgibbs@gmail.com>
2011-09-21 10:52:50 -0500

    Create file utils class

---
+++ xDrive/FileUtils.h
@@ -0,0 +1,11 @@
+//
+//  FileUtils.h
+//  xDrive
+//
+//  Created by Chris Gibbs on 9/21/11.
+//  Copyright (c) 2011 Abilene Christian University. All rights reserved.
+//
+
+@interface FileUtils : NSObject
+
+@end
---
+++ xDrive/FileUtils.m
@@ -0,0 +1,13 @@
+//
+//  FileUtils.m
+//  xDrive
+//
+//  Created by Chris Gibbs on 9/21/11.
+//  Copyright (c) 2011 Abilene Christian University. All rights reserved.
+//
+
+#import "FileUtils.h"
+
+@implementation FileUtils
+
+@end
