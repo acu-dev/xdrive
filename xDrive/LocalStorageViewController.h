ded5e0817858cf76e3d6859b6002462b757d0005
Chris Gibbs <cjgibbs@gmail.com>
2011-09-30 14:40:11 -0500

    Add local storage selector view

---
+++ xDrive/LocalStorageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocalStorageViewController.h
+//  xDrive
+//
+//  Created by Chris Gibbs on 9/30/11.
+//  Copyright (c) 2011 Abilene Christian University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalStorageViewController : UITableViewController
+
+@end
