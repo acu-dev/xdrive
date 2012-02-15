7a51eb30daf3b9024815b629a4e268426ddf3a0e
Chris Gibbs <cjgibbs@gmail.com>
2012-02-15 13:45:39 -0600

    add size and size description to file object

--- xDrive/XFile.h
+++ xDrive/XFile.h
@@ -2,17 +2,20 @@
 //  XFile.h
 //  xDrive
 //
-//  Created by Chris Gibbs on 7/5/11.
-//  Copyright (c) 2011 Abilene Christian University. All rights reserved.
+//  Created by Chris Gibbs on 2/15/12.
+//  Copyright (c) 2012 Meld Apps. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import "XEntry.h"
 
 
 @interface XFile : XEntry
 
-@property (nonatomic, retain) NSString * size;
+@property (nonatomic, retain) NSNumber * size;
 @property (nonatomic, retain) NSString * type;
+@property (nonatomic, retain) NSString * sizeDescription;
 
 - (NSString *)extension;
 - (NSString *)cachePath;
--- xDrive/XFile.m
+++ xDrive/XFile.m
@@ -2,19 +2,19 @@
 //  XFile.m
 //  xDrive
 //
-//  Created by Chris Gibbs on 7/5/11.
-//  Copyright (c) 2011 Abilene Christian University. All rights reserved.
+//  Created by Chris Gibbs on 2/15/12.
+//  Copyright (c) 2012 Meld Apps. All rights reserved.
 //
 
 #import "XFile.h"
 #import "XService.h"
-#import "XDriveConfig.h"
 
 
 @implementation XFile
 
 @dynamic size;
 @dynamic type;
+@dynamic sizeDescription;
 
 - (NSString *)extension
 {
