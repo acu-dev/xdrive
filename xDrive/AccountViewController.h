ec58312b1932107b5b92e2f19009593549a871d7
Chris Gibbs <cjgibbs@gmail.com>
2012-02-16 10:54:30 -0600

    fix copyright holder

--- xDrive/AccountViewController.h
+++ xDrive/AccountViewController.h
@@ -3,7 +3,7 @@
 //  xDrive
 //
 //  Created by Chris Gibbs on 2/15/12.
-//  Copyright (c) 2012 Meld Apps. All rights reserved.
+//  Copyright (c) 2012 Abilene Christian University. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
