565db050188fc71c42fb8bb156a953370eaed51d
Chris Gibbs <cjgibbs@gmail.com>
2012-03-31 19:20:24 -0500

    fixed fetched results controller and cleanup code

--- xDrive/RecentViewController.m
+++ xDrive/RecentViewController.m
@@ -9,6 +9,7 @@
 #import "RecentViewController.h"
 #import "XService.h"
 #import "OpenFileViewController.h"
+#import "XDriveConfig.h"
 
 
 
@@ -16,61 +17,33 @@
 
 @property (nonatomic, strong) NSFetchedResultsController *_fetchedResultsController;
 
-@property (nonatomic, strong) NSDictionary *iconTypes;
-
 @end
-
-
-
-
 
 
 @implementation RecentViewController
 
-// Private
 @synthesize _fetchedResultsController;
-@synthesize iconTypes;
 
 
-
-- (id)init
-{
-	self = [super init];
-	if (!self) return nil;
-	
-	_fetchedResultsController = [[XService sharedXService].localService recentlyAccessedFilesController];
-	_fetchedResultsController.delegate = self;
-	
-	return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-	self.iconTypes = nil;
-}
 
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+	
+	_fetchedResultsController = [[XService sharedXService].localService recentlyAccessedFilesController];
+	_fetchedResultsController.delegate = self;
+	NSError *error = nil;
+	if (![_fetchedResultsController performFetch:&error])
+	{
+		XDrvLog(@"Error getting recent files: %@", error);
+	}
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -89,55 +62,11 @@
 
 #pragma mark - Cell customization
 
-- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+- (void)configureCell:(UITableViewCell *)cell forFile:(XFile *)file
 {
-    XEntry *entry = [_fetchedResultsController objectAtIndexPath:indexPath];
-    [self configureCell:cell forEntry:entry];
-}
-
-- (void)configureCell:(UITableViewCell *)cell forEntry:(XEntry *)entry
-{
-	cell.textLabel.text = entry.name;
-	
-	NSString *type = nil;
-	if ([entry isKindOfClass:[XFile class]])
-	{
-		cell.detailTextLabel.text = ((XFile *)entry).sizeDescription;
-		type = ((XFile *)entry).type;
-	}
-	else
-	{
-		type = @"folder";
-	}
-	
-	cell.imageView.image = [self iconForEntryType:type];
-}
-
-- (UIImage *)iconForEntryType:(NSString *)entryType
-{
-	if (!iconTypes)
-	{
-		// Load icon mappings from plist
-		iconTypes = [[NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"File-Types" ofType:@"plist"]] objectForKey:@"Icons"];
-	}
-	
-	// First check for exact match
-	NSString *iconName = [iconTypes objectForKey:entryType];
-	if (iconName)
-	{
-		return [UIImage imageNamed:iconName];
-	}
-	
-	// Match type category
-	NSString *category = [[entryType componentsSeparatedByString:@"/"] objectAtIndex:0];
-	iconName = [iconTypes objectForKey:category];
-	if (iconName)
-	{
-		return [UIImage imageNamed:iconName];
-	}
-	
-	// Use default
-	return [UIImage imageNamed:[iconTypes objectForKey:@"default"]];
+	cell.textLabel.text = file.name;
+	cell.detailTextLabel.text = file.sizeDescription;
+	cell.imageView.image = [UIImage imageNamed:[[XService sharedXService] iconNameForEntryType:file.type]];
 }
 
 
@@ -158,20 +87,12 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    XEntry *entry = [_fetchedResultsController objectAtIndexPath:indexPath];
+    XFile *file = [_fetchedResultsController objectAtIndexPath:indexPath];
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-	[self configureCell:cell forEntry:entry];
+	[self configureCell:cell forFile:file];
 	return cell;
 }
 
-
-
-#pragma mark - Table view delegate
-
-/*- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-	[tableView deselectRowAtIndexPath:indexPath animated:YES];
-}*/
 
 
 #pragma mark - NSFetchedResultsControllerDelegate
@@ -214,7 +135,6 @@
             break;
             
         case NSFetchedResultsChangeUpdate:
-            //[self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
 			[tableView cellForRowAtIndexPath:indexPath];
             break;
             
@@ -229,17 +149,6 @@
 {
     [self.tableView endUpdates];
 }
-
-/*
- // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed. 
- 
- - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
- {
- // In the simplest, most efficient, case, reload the table view.
- [self.tableView reloadData];
- }
- */
-
 
 @end
 
